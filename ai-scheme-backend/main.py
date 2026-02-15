0241bd1311abcc60d711806105fb911a946b9e15
Riyanshu Kumar <riyanshukumar@192.168.1.22>
2026-02-16 01:12:03 +0530

    update cors

--- ai-scheme-backend/main.py
+++ ai-scheme-backend/main.py
@@ -11,7 +11,7 @@ init_db()
 # ✅ CORS — SIMPLE & CORRECT
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://ai-powered-voice-first-conversation.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
